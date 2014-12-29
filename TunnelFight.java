9ab9b049242f055741f35ee51602ceb2aaba8ce4
Gordan Sajević <gsajevic@hotmail.com>
2014-12-29 22:10:09 +0100

    Update TunnelFight.java

--- TunnelFight.java
+++ TunnelFight.java
@@ -3,19 +3,23 @@ public class TunnelFight {
 
 	//Testna klasa
 	
+	static GameObject winner;
+	
 	public static void main(String[] args) {
 		Dwarf obrim = new Dwarf("Obrim, sin Agammov", 9, 30, 2);
-		GameObject[] orcs = new Orc[10];
-		for (int i=0; i<orcs.length-1; i++)
+		GameObject[] enemy = new GameObject[10];
+		for (int i=0; i<enemy.length-1; i++)
 		{
-			orcs[i] = new Orc(5, 10);
+			enemy[i] = new Orc(5, 10);
 		}
 		
-		//Anonimna klasa
-		orcs[orcs.length-1] = new Orc(3, 3){
-			
+		//Anonimna klasa za goblina
+		
+		enemy[enemy.length-1] = new Enemy(3.0, 3.0)
+		{
+			@Override
 			public void handleAttack(double attack) {
-				this.health -= attack*2;
+				this.health -= 2*attack;
 				
 			}
 			
@@ -24,13 +28,16 @@ public class TunnelFight {
 				return "Goblin " + "\nAttack strength: " + attackStrength + "\nHealth: " + health;
 			}
 		};
-		int numOrcs = orcs.length;
+		int numOrcs = enemy.length;
 		int i=0;
 		while (obrim.getHealth() > 0 && numOrcs > 0 )
 		{
-			Duel battle = new Duel(obrim, orcs[i]);
-			battle.fight();
-			numOrcs--;
+			Duel battle = new Duel(obrim, enemy[i]);
+			winner = battle.fight();
+			if (winner == obrim)
+			{
+				numOrcs--;
+			}
 			i++;
 		}
 
@@ -40,3 +47,4 @@ public class TunnelFight {
 	}
 
 }
+
