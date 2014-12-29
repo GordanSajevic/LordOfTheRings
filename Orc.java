e4af628a88461a306f7088a35c071ca75014fff6
Gordan Sajević <gsajevic@hotmail.com>
2014-12-29 22:10:31 +0100

    Update Orc.java

--- Orc.java
+++ Orc.java
@@ -1,109 +1,23 @@
-public class Orc implements GameObject{
 
-	protected double attackStrength;
-	protected double health;
-	
+public class Orc extends Enemy implements GameObject{
+
 	/**
 	 * Konstruktor sa dva parametra
 	 * @param attackStrength
 	 * @param health
 	 */
 	
-	public Orc(double attackStrength, double health)
-	{
-		setAttackStrength(attackStrength);
-		setHealth(health);
+	public Orc(double attackStrength, double health) {
+		super(attackStrength, health);
+		this.attackStrength = attackStrength;
+		this.health = health;
 	}
 	
 	/**
-	 * Getter za attackStrength
-	 * @return attackStrength
+	 * Override metode toString
 	 */
 	
-	public double getAttackStrength()
-	{
-		return attackStrength;
-	}
-	
-	/**
-	 * Getter za health
-	 * @return health
-	 */
-	
-	public double getHealth()
-	{
-		return health;
-	}
-	
-	/**
-	 * Setter za attackStrength
-	 * @param attackStrength
-	 */
-	
-	public void setAttackStrength(double attackStrength)
-	{
-		if (attackStrength <= 0)
-		{
-			throw new IllegalArgumentException("Attack strength cannot be lesser or equal to zero!");
-		}
-		else
-		{
-			this.attackStrength = attackStrength;
-		}
-	}
-	
-	/**
-	 * Setter za health
-	 * @param health
-	 */
-	
-	public void setHealth(double health)
-	{
-		if (health <= 0)
-		{
-			throw new IllegalArgumentException("Health cannot be lesser or equal to zero!");
-		}
-		else
-		{
-			this.health= health;
-		}
-	}
-	
-	/**
-	 * Implementacija metode isAlive iz GameObject interfacea
-	 * return boolean
-	 */
-	
-	public boolean isAlive() {
-		if (this.health > 0)
-		{
-			return true;
-		}
-		return false;
-	}
-	
-	/**
-	 * Implementacija metode attack iz GameObject interfacea
-	 */
-	
-	public void attack(GameObject other) {
-		other.handleAttack(attackStrength);
-		
-	}
-	
-	/**
-	 * Implementacija metode handleAttack iz GameObject interfacea
-	 */
-	
-	public void handleAttack(double attack) {
-		this.health -= attack;
-		
-	}
-
-	/**
-	 * Metoda koja vraća string sa svim podacima o objektu
-	 */
-	
+	@Override
 	public String toString()
 	{
 		return "Orc " + "\nAttack strength: " + attackStrength + "\nHealth: " + health;
@@ -111,3 +25,5 @@ public class Orc implements GameObject{
 	
 	
 }
+
+	
